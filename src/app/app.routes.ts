4e2636e4dfe5b402367d8f9f837cae02d9776d70
Yessie <yaddaif@outlook.com>
2025-06-03 20:54:59 +0200

    about-us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { List } from './menu/list/list';
 import { Home } from './home/home';
+import { Summary } from './about-us/summary/summary';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'menu', component: List },
+  { path: 'about-us', component: Summary },
 ];
