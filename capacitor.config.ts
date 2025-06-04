6b147a8d580004688ccdf0d01cda80c6951d76d7
Yassine Ed-daïf <94546645+Yessie1601@users.noreply.github.com>
2025-06-04 14:40:24 +0200

    Make website responsive and create APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'LoremIpsum',
-  webDir: 'dist'
+  appId: 'com.loremipsum.app',
+  appName: 'Lorem Ipsum',
+  webDir: 'dist/LoremIpsum/browser',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#FFFFFF",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP"
+    }
+  }
 };
 
 export default config;
